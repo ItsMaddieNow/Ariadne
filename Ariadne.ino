abb2cc079972c330287a594fdeebd7990f961953
mariowhitmore <36354501+mariowhitmore@users.noreply.github.com>
2020-08-03 13:01:45 +1200

    Various Boiler Plate

--- Ariadne.ino
+++ Ariadne.ino
@@ -1,9 +1,34 @@
+//Libraries
+
+#include <Servo.h>
+
+// Motor Pins
+int RightMotorForwardPin = 0;
+int RightMotorBackwardPin = 0;
+int LeftMotorForwardPin = 0;
+int LeftMotorBackwardPin = 0;
+// Ultra Sonic Sensor
+int UltraSonicPinTrig = 0;
+int UltraSonicPinEcho = 0;
+int ServoPin = 0;
+// Line Following Sensor
+int RightSensorPin = 0;
+int CenterSensorPin = 0;
+int LeftSensorPin = 0;
+// Button Pins
+int RightButtonPin = 0;
+int LeftButtonPin = 0;
+// LED Pins
+int RightLEDPin = 0;
+int CenterLEDPin = 0;
+int LeftLEDPin = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
