f2213781c42bad79604d6fdff24aafbee62a4943
mariowhitmore <mariolwhitmore@gmail.com>
2020-08-10 16:51:57 +1200

    Placed LED Blinking on start in its own method

--- Ariadne.ino
+++ Ariadne.ino
@@ -3,27 +3,27 @@
 #include <Servo.h>
 
 // Motor Pins
-int RightMotorForwardPin = 5;
-int RightMotorBackwardPin = 6;
-int LeftMotorForwardPin = 10;
-int LeftMotorBackwardPin = 9;
+const int RightMotorForwardPin = 5;
+const int RightMotorBackwardPin = 6;
+const int LeftMotorForwardPin = 10;
+const int LeftMotorBackwardPin = 9;
 // Servo Pins
-int ServoPin = 12;
+const int ServoPin = 12;
 Servo UltraSonicServo;
 // Ultra Sonic Sensor
-int UltraSonicPinTrig = 0;
-int UltraSonicPinEcho = 0;
+const int UltraSonicPinTrig = 0;
+const int UltraSonicPinEcho = 0;
 // Line Following Sensor
-int RightSensorPin = 0;
-int CenterSensorPin = 0;
-int LeftSensorPin = 0;
-// Button Pins
-int RightButtonPin = 0;
-int LeftButtonPin = 0;
+const int RightSensorPin = 0;
+const int CenterSensorPin = 0;
+const int LeftSensorPin = 0;
+// Buttons
+const int RightButtonPin = 0;
+const int ButtonState;
 // LED Pins
-int RightLEDPin = 0;
-int StartLEDPin = 13;
-int LeftLEDPin = 0;   
+const int RightLEDPin = 0;
+const int StartLEDPin = 13;
+const int LeftLEDPin = 0;   
 
 void setup() {
   // put your setup code here, to run once:
@@ -33,21 +33,24 @@ void setup() {
 
   pinMode(StartLEDPin, OUTPUT);
   
-  digitalWrite(StartLEDPin, HIGH);
-  delay(500);
-  digitalWrite(StartLEDPin, LOW);
-  delay(500);
-  digitalWrite(StartLEDPin, HIGH);
-  delay(500);
-  digitalWrite(StartLEDPin, LOW);
-  delay(500);
-  digitalWrite(StartLEDPin, HIGH);
+  BlinkStartLed();
   
   digitalWrite(5, HIGH);
   digitalWrite(10, HIGH);
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void BlinkStartLed() {
+  digitalWrite(StartLEDPin, HIGH);
+  delay(500);
+  digitalWrite(StartLEDPin, LOW);
+  delay(500);
+  digitalWrite(StartLEDPin, HIGH);
+  delay(500);
+  digitalWrite(StartLEDPin, LOW);
+  delay(500);
+  digitalWrite(StartLEDPin, HIGH);
 }
